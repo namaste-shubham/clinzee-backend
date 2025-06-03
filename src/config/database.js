e3a33d24c25cfa49b0b0534b564a601a0dd37c5c
Shubham Kumar <shubham20.kumar@lge.com>
2025-06-03 21:16:08 +0900

    fix: read mongodb url from env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://jyoti-enterprises:2at10r8lWtf1PYfQ@clinzee.vly55kt.mongodb.net/clinzee"
-  );
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = connectDB;
